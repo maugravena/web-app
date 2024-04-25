71a08617b4b181ed305685c94cf85de6366591a1
Mauricio Gravena <mauricio.gravena@rebase.com.br>
2024-04-25 18:33:40 -0300

    add jwt for the policies index

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,3 +1,38 @@
 class PoliciesController < ApplicationController
-  def index; end
+  def index
+    encoded_token = JWT.encode({}, ENV['SECRET_KEY'], 'HS256')
+
+    url = URI.parse('http://rails-graphql:9999/graphql')
+    http = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Post.new(url.request_uri)
+    request.body = { query: graphql_query }.to_json
+    request['Authorization'] = encoded_token
+    request['Content-Type'] = 'application/json'
+    response = http.request(request)
+
+    @policies = JSON.parse(response.body)['data']['policies']
+  end
+
+  private
+
+  def graphql_query
+    <<-GRAPHQL
+      query {
+        policies {
+          policyId
+          emissionDate
+          endDateCoverage
+          vehicle {
+            brand
+            model
+            year
+          }
+          insured {
+            name
+            cpf
+          }
+        }
+      }
+    GRAPHQL
+  end
 end
